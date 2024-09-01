ec1bc5ea05f126e6efc0b4c0f536605488a45658
husainwafaie <117494930+husainwafaie@users.noreply.github.com>
2024-08-31 20:32:15 -0700

    Login Implemented and Home Page

--- musicrecommender/recommender/views.py
+++ musicrecommender/recommender/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
+def index(request):
+    return redirect(login_view)
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(mainpage)
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+@login_required
+def mainpage(request):
+    return render(request, "homepage.html")
