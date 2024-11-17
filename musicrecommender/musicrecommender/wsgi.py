c6ef22f5434ccb4bbb76cd25cf3b97548d7db56e
husainwafaie <117494930+husainwafaie@users.noreply.github.com>
2024-11-17 11:07:59 -0800

    Added vercel configuration

--- musicrecommender/musicrecommender/wsgi.py
+++ musicrecommender/musicrecommender/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'musicrecommender.settings')
 
 application = get_wsgi_application()
+app = application
